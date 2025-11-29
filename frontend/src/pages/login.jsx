1e9490d4bfaf5bb2d3e9a124dc876706c43b7a92
Sohan <sohan291106@gmail.com>
2025-11-29 17:07:11 +0530

    added toggle password

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import Header from "../components/header";
 
 const LoginForm = () => {
+    const [view, setview] = useState("🤫");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const viewPassword = () => {
+        const passwordInput = document.getElementById("password");
+        if (view === "🤫") {
+            passwordInput.value="·".repeat(passwordInput.value.length);
+            setview("🫣");
+        } else {
+            passwordInput.value = password;
+            setview("🤫")
+        };
+    }
+    const submitData = (e) => {
+        e.preventDefault();
+        console.log({email, password});
+    }
     return (
-        <form className="flex flex-col md:w-lg lg:w-xl gap-4 bg-gray-700 p-8 rounded-lg shadow-lg">
+        <form className="flex flex-col md:w-lg lg:w-xl gap-4 bg-gray-700 p-8 rounded-lg shadow-lg" onSubmit={submitData}>
             <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
-            <label className="text-white text-lg font-medium" for="mail">Mail:</label>
-            <input className="p-2 rounded-md text-white bg-gray-600 sm:w-sm md:w-xs lg:w-sm" type="email" id="mail" name="mail" required />
+                <label className="text-white text-lg font-medium" htmlFor="mail">Mail:</label>
+                <input className="p-2 rounded-md text-white bg-gray-600 sm:w-sm md:w-xs lg:w-sm" type="email" id="mail" name="mail" value={email} onChange={(e)=>setEmail(e.target.value)} required />
             </div>
-            <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
-            <label className="text-white text-lg font-medium" for="password">Password:</label>
-            <input className="p-2 rounded-md text-white bg-gray-600 sm:w-sm md:w-xs lg:w-sm" type="password" id="password" name="password" required />
+            <div className="relative flex flex-col sm:flex-row sm:justify-between sm:items-center">
+                <label className="text-white text-lg font-medium" htmlFor="password">Password:</label>
+                <input className="p-2 rounded-md text-white bg-gray-600 sm:w-sm md:w-xs lg:w-sm" type="text" id="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)} required />
+                <span className="absolute top-8.5 right-2 sm:top-2 sm:right-3 cursor-pointer" onClick={viewPassword}>{view}</span>
             </div>
             <button className="bg-amber-200 text-gray-800 font-bold py-2 px-4 rounded-md hover:bg-amber-300 mt-4" type="submit">Login</button>
         </form>
