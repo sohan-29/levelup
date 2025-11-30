5c7b3acfbb4bcf6cbb8fed523a90bdb21c0c8d98
Sohan <sohan291106@gmail.com>
2025-11-30 21:38:16 +0530

    corrected view toggling in signup page

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -3,18 +3,30 @@ import Header from "../components/header";
 
 const SignupForm = () => {
     const [view, setview] = useState("🤫");
+    const [confirmView, setConfirmView] = useState("🤫");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const viewPassword = (id) => {
-        const passwordInput = document.getElementById(id);
-        if (view === "🤫") {
-            passwordInput.value = "·".repeat(passwordInput.value.length);
-            setview("🫣");
+        if (id === "password") {
+            const passwordInput = document.getElementById(id);
+            if (view === "🤫") {
+                passwordInput.value = "·".repeat(passwordInput.value.length);
+                setview("🫣");
+            } else {
+                passwordInput.value = password;
+                setview("🤫")
+            }
         } else {
-            passwordInput.value = password;
-            setview("🤫")
-        };
+            const passwordInput = document.getElementById(id);
+            if (confirmView === "🤫") {
+                passwordInput.value = "·".repeat(passwordInput.value.length);
+                setConfirmView("🫣");
+            } else {
+                passwordInput.value = confirmPassword;
+                setConfirmView("🤫")
+            }
+        }
     }
     const submitData = (e) => {
         e.preventDefault();
@@ -34,7 +46,7 @@ const SignupForm = () => {
             <div className="relative flex flex-col sm:flex-row sm:justify-between sm:items-center">
                 <label className="text-white text-lg font-medium w-1/4" htmlFor="password">Confirm Password:</label>
                 <input className="p-2 rounded-md text-white bg-gray-600 sm:w-sm md:w-xs lg:w-sm" type="text" id="confirmPassword" name="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
-                <span className="absolute top-8.5 right-2 sm:top-4 sm:right-3 cursor-pointer" onClick={() => viewPassword("confirmPassword")}>{view}</span>
+                <span className="absolute top-8.5 right-2 sm:top-4 sm:right-3 cursor-pointer" onClick={() => viewPassword("confirmPassword")}>{confirmView}</span>
                 {password != confirmPassword && <span className="text-red-600 absolute top-14 text-center w-full">password not matched !</span>}
             </div>
             <button className={`bg-[#fee369] text-gray-800 font-bold py-2 px-4 rounded-md mt-5 ${password != confirmPassword ? "cursor-not-allowed" : "hover:bg-amber-300"}`} type="submit">Signup</button>
