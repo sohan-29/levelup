a89d73207100db42aceade5a653fadbcf54a6817
Sohan <sohan291106@gmail.com>
2025-12-09 18:19:51 +0530

    modified cors

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,10 @@ const PORT = 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
+origin: 'http://localhost:5173',
+credentials: true,
+allowedHeaders: ['Content-Type', 'Authorization'],
+methods: ['GET','POST','PUT','DELETE','OPTIONS']
 }));
 
 app.use(express.json());
